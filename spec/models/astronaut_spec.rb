c42981a12e236ed8d8a0876120c27d457ca49cb2
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-07-06 10:45:57 -0600

    Initial seeding complete

---
+++ spec/models/astronaut_spec.rb
@@ -0,0 +1,7 @@
+describe Astronaut, type: :model do
+  describe 'validations' do
+    it {should validate_presence_of(:name)}
+    it {should validate_presence_of(:age)}
+    it {should validate_presence_of(:job)}
+  end
+end
