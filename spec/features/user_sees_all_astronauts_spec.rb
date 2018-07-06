206bf05b42bb495b2ff6fd8fa92728747e6083d9
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-07-06 11:07:40 -0600

    poor form - made feature test before model test... tsktsk

---
+++ spec/features/user_sees_all_astronauts_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe 'user-index' do
+  it 'can see all astronauts and their respective info' do
+    astronaut_1 = Astronaut.create(name: "Buzz Lightyear of Star Command", age: 34, job: 'Arch-nemesis of Emperor Zerg')
+    astronaut_2 = Astronaut.create(name: "Boba Fett", age: 32, job: 'Bounty Hunter for Jabba the Hutt')
+
+    visit '/astronauts'
+
+    expect(page).to have_content(astronaut_1.name)
+    expect(page).to have_content(astronaut_1.age)
+    expect(page).to have_content(astronaut_1.job)
+    expect(page).to have_content(astronaut_2.name)
+    expect(page).to have_content(astronaut_2.age)
+    expect(page).to have_content(astronaut_2.job)
+  end
+  it 'can see the average age of all astronauts' do
+    astronaut_1 = Astronaut.create(name: "Buzz Lightyear of Star Command", age: 10, job: 'Arch-nemesis of Emperor Zerg')
+    astronaut_2 = Astronaut.create(name: "Boba Fett", age: 20, job: 'Bounty Hunter for Jabba the Hutt')
+
+    visit '/astronauts'
+
+    expect(page).to have_content(15)
+  end
+end
--- spec/models/astronaut_spec.rb
+++ spec/models/astronaut_spec.rb
@@ -1,5 +1,9 @@
+require 'rails_helper'
+
 describe Astronaut, type: :model do
   describe 'validations' do
+    astronaut = Astronaut.create(name: "Buzz Lightyear of Star Command", age: 34, job: 'Arch-nemesis of Emperor Zerg')
+
     it {should validate_presence_of(:name)}
     it {should validate_presence_of(:age)}
     it {should validate_presence_of(:job)}
